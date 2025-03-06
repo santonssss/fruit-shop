9a00c7c2b31cb1b1745f0d5897fc66578b9ecd8c
santonssss <sarvarkalmuratov370@gmail.com>
2025-03-07 02:38:02 +0500

    feat: project base setup and styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@data": path.resolve(__dirname, "./src/data"),
+      "@context": path.resolve(__dirname, "./src/context"),
+      "@store": path.resolve(__dirname, "./src/store"),
+      "@lib": path.resolve(__dirname, "./src/lib"),
+    },
+  },
+});
