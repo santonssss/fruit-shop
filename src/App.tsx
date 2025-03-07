90afc4c40959c28cb210ef887a4cef12438fc288
santonssss <sarvarkalmuratov370@gmail.com>
2025-03-07 14:42:08 +0500

    feat:add route to about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import Shop from "./pages/Shop";
+import About from "./pages/About";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="/products" element={<Shop />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
