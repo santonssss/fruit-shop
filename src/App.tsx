8a86bc33434a66439b38b08281e7d11536052b48
santonssss <sarvarkalmuratov370@gmail.com>
2025-03-08 00:19:11 +0500

    refactor: wrapped entire app in Layout component

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,27 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Shop from "./pages/Shop";
 import About from "./pages/About";
+import ProductDetail from "./pages/ProductDetail";
+import { Toaster } from "@components/ui/toaster";
+import { Toaster as Sonner } from "@components/ui/sonner";
+import { TooltipProvider } from "@components/ui/tooltip";
+import Layout from "@components/Layout";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/products" element={<Shop />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <TooltipProvider>
+      <BrowserRouter>
+        <Toaster />
+        <Sonner />
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/products" element={<Shop />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </TooltipProvider>
   );
 }
 
