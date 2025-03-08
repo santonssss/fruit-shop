1eddf19ebc9f809ee8bde027da7e352042865581
santonssss <sarvarkalmuratov370@gmail.com>
2025-03-08 12:12:28 +0500

    feat: added products page

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import { Toaster } from "@components/ui/toaster";
 import { Toaster as Sonner } from "@components/ui/sonner";
 import { TooltipProvider } from "@components/ui/tooltip";
 import Layout from "@components/Layout";
+import Cart from "./pages/Cart";
+import Contact from "./pages/Contacts";
 function App() {
   return (
     <TooltipProvider>
@@ -20,6 +22,8 @@ function App() {
             <Route path="/products" element={<Shop />} />
             <Route path="/about" element={<About />} />
             <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </Layout>
       </BrowserRouter>
